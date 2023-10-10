af415dbe6ee1c22c35ab0785cc4e3ba59aec9a6f
nastya <146085576+catcupppp@users.noreply.github.com>
2023-10-10 14:37:31 +0500

    Add files via upload

---
+++ my-first-project/src/main/java/com/example/myfirstproject/telegram/MyAmazingBot.java
@@ -0,0 +1,35 @@
+package com.example.myfirstproject.telegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "myFirstSunBot";
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return "6666777222:AAGew0b7KZIPufuZLGQzcmX63fxUC6wq5Vw";
+    }
+}
